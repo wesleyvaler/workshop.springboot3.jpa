837997beab37e77e6aa50da0f4d9dc32ef303edc
wesleyvaler <wesley.ufrb@gmail.com>
2023-08-24 16:33:08 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -21,10 +21,10 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-
 		return obj.get();
 	}
 	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
-				
-	
